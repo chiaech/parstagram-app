c92f716a8a9457b479293d39264fdddac9938416
chia <38141245+chiaech@users.noreply.github.com>
2019-11-15 21:55:10 -0800

    Add files via upload

---
+++ parstagram-app/PostCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostCell.swift
+//  parstagram-app
+//
+//  Created by chia seed on 11/14/19.
+//  Copyright © 2019 Chia E. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
